805a6576424ed3aec0195a302e469fb7f1ed64ee
Rick Sanchez <matinbeigi99@gmail.com>
2025-04-02 08:44:09 +0330

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,9 @@
+// analytics.js
+(function () {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.counter.dev/script.js';
+    script.setAttribute('data-id', '42aac39f-23a3-416e-a788-12c3bafefb62');
+    script.setAttribute('data-utcoffset', '4');
+    script.async = true;
+    document.head.appendChild(script);
+})();
